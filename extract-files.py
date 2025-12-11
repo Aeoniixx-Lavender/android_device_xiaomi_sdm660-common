3874a3de64b042a6faf777f9b5bc3ffb39e686fa
Michael Bestas <mkbestas@lineageos.org>
2025-12-11 13:30:36 +0200

    sdm660-common: Patch some blobs to depend on libtinyxml2_1.so
    libtinyxml2 10.1.0 update which landed in BP4A contains ABI
    incompatible changes which cause runtime crashes.
    Since the lib lives in system, we can't easily blob it,
    however we can use qualcomm's libtinyxml2_1.so that already exists
    in vendor and seems to work as expected.
    
    Change-Id: Icc67da5f754b0bb30dce6192b1e520fdf1349ac5
    Signed-off-by: Aeoniixx <aeoniixx@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -121,7 +121,9 @@ blob_fixups: blob_fixups_user_type = {
         .fix_soname(),
     'system_ext/lib64/libqxrsplitauxservice.qti.so': blob_fixup()
         .add_needed('libaudioclient_shim.so')
-        .add_needed('libwfdservice_shim.so')
+        .add_needed('libwfdservice_shim.so'),
+    'vendor/bin/mm-pp-dpps': blob_fixup()
+        .replace_needed('libtinyxml2.so', 'libtinyxml2_1.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
