46697e6bf509d4da7a7d82cd0df19d7c4eaff642
Aeoniixx <aeoniixx@gmail.com>
2026-01-17 06:36:51 +0000

    sdm660-common: Switch libperfmgr paths to VoltageOS
    Signed-off-by: Aeoniixx <aeoniixx@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -573,7 +573,7 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
     hardware/google/pixel \
     hardware/xiaomi \
-    hardware/lineage/interfaces/power-libperfmgr \
+    hardware/voltage/interfaces/power-libperfmgr \
     hardware/qcom-caf/common/libqti-perfd-client
 
 # Speed profile services and wifi-service to reduce RAM and storage
